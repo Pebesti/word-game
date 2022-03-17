44f031d89fce5e901c254d26450764291c164c3d
Pebesti <82022148+Pebesti@users.noreply.github.com>
2022-03-17 16:51:26 +0200

    wordgames

--- word.js
+++ word.js
@@ -1,27 +1,13 @@
 let check = document.querySelector(".btn");
 
-check.addEventListener("click", btnClicked);
+let checkbox = document.getElementById("checkbox");
 
-function highlight_words(elem, regex) {
-    [].forEach.call(elem, function(ele) {
-        ele.innerHTML = ele.innerHTML.replace(regex, '<span>$&</span>');
-    })
+checkbox.addEventListener("click", function(){
+    var checkedRadioBtn = document.querySelector("input[name='Hide']:checked");
+    if (checkedRadioBtn){
 
-}
-
-function creatPar() {
-    var par = document.createElement('p');
-    var inp = document.querySelector('.words').value;
-    par.innerHTML = inp;
-    par.classList.add('highlight');
-    var div = document.getElementById('word');
-    div.appendChild(par);
-}
-
-function btnClicked() {
-    creatPar();
-    highlight_words(document.querySelectorAll('p'), /[a-zA-Z]{5,}/g);
-}let check = document.querySelector(".btn");
+    }
+});
 
 check.addEventListener("click", btnClicked);
 
